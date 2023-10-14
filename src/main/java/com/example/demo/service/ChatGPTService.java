c561e624d0658df9acc58e0c19a03b6658f78a05
ransmin2 <50533122+ransmin2@users.noreply.github.com>
2023-10-15 00:41:44 +0100

    Update ChatGPTService.java

--- src/main/java/com/example/demo/service/ChatGPTService.java
+++ src/main/java/com/example/demo/service/ChatGPTService.java
@@ -7,7 +7,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
 import java.util.Collections;
 
 @Service
